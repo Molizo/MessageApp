62c4a03d708cbb5061f99e74fdf862739c6fc2a6
Visoiu Mihnea Theodor <visoiu.mihnea@gmail.com>
2018-07-09 13:28:40 +0300

    Added Server Databases and IPV4 Address Functionality.

--- MessageServer/ServerMain.cs
+++ MessageServer/ServerMain.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace MessageServer
 {
@@ -15,6 +17,27 @@ namespace MessageServer
         public ServerMain()
         {
             InitializeComponent();
+            labelIP.Text = LocalIPAddress().ToString();
+        }
+
+        private void ServerMain_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'messageDbDataSet1.Table' table. You can move, or remove it, as needed.
+            this.tableTableAdapter.Fill(this.messageDbDataSet1.Table);
+        }
+
+        private IPAddress LocalIPAddress()
+        {
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                return null;
+            }
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+            return host
+                .AddressList
+                .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
         }
     }
 }
