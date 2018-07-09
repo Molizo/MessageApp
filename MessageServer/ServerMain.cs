fc9450b5f0fcac522adbbc30182ca9d278e9cd15
Visoiu Mihnea Theodor <visoiu.mihnea@gmail.com>
2018-07-09 12:58:14 +0300

    Added Solutions and Project files

---
+++ MessageServer/ServerMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageServer
+{
+    public partial class ServerMain : Form
+    {
+        public ServerMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
