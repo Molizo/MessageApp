7db3f1c5bf29c5318d773cc5294615c8515a845c
Visoiu Mihnea Theodor <visoiu.mihnea@gmail.com>
2018-07-13 17:12:19 +0300

    Comments added to client message list viewer form

--- MessageApp/MessageListViewer.cs
+++ MessageApp/MessageListViewer.cs
@@ -5,21 +5,21 @@ namespace MessageApp
 {
     public partial class MessageListViewer : Form
     {
-        public MessageListViewer()
+        public MessageListViewer() //Initializes stuff
         {
             InitializeComponent();
         }
 
-        public void OpenFile(string fileName)
+        public void OpenFile(string fileName) //Opens a file for viewing
         {
             this.Text = "Message file viewer - " + Path.GetFileName(fileName) + " - Messaging Client";
-            if (Path.GetExtension(fileName) == ".msgusr")
+            if (Path.GetExtension(fileName) == ".msgusr") //Add columns used in a .msgusr file
             {
                 dataGridViewMessages.Columns.Add("columnTimestamp", "Timestamp");
                 dataGridViewMessages.Columns.Add("columnSender", "From");
                 dataGridViewMessages.Columns.Add("columnMessage", "Message");
             }
-            else if (Path.GetExtension(fileName) == ".msgdat")
+            else if (Path.GetExtension(fileName) == ".msgdat") //Add columns used in a .msgdat file
             {
                 dataGridViewMessages.Columns.Add("columnSender", "Sender");
                 dataGridViewMessages.Columns.Add("columnReceiver", "Receiver");
@@ -29,9 +29,10 @@ namespace MessageApp
                 MessageBox.Show("Error opening the file.", "File operations Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             try
             {
+                //Reads the file
                 System.IO.StreamReader file = new System.IO.StreamReader(fileName);
                 string line;
-                while ((line = file.ReadLine()) != null)
+                while ((line = file.ReadLine()) != null) //Processes the data and adds it to the dataGridViewMessages table
                 {
                     string[] data = line.Split(',');
                     data[0] = data[0].Trim('\'');
